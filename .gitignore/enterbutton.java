980c2567ce51c3587d8ac1f64d05ef8fdaac1381
RisingSmurf <30986070+RisingSmurf@users.noreply.github.com>
2017-08-14 10:32:22 +0200

    Update enterbutton.java

--- .gitignore/enterbutton.java
+++ .gitignore/enterbutton.java
@@ -1,17 +1,16 @@
     public void WalkToWild() {
-    	
-    	RS2Widget enterButton = getWidgets().getWidgetContainingText("Enter Wilderness");
-    	if (!wilderness.contains(myPlayer().getPosition())) {
-            WebWalkEvent webWalkEvent = new WebWalkEvent(wilderness);
-            webWalkEvent.setBreakCondition(new Condition() {
-                @Override
-                public boolean evaluate() {
-                    return enterButton != null && enterButton.isVisible();
-                }
-            });
-    	getWalking().webWalk(wilderness);
-    	} if (enterButton != null && enterButton.isVisible()){
-    	enterButton.interact("Enter Wilderness"); 
-    	}
-    	
-    }
+            RS2Widget enterWildernessButton = getWidgets().singleFilter(getWidgets().getAll(), new WidgetActionFilter("Enter Wilderness"));
+            if (enterWildernessButton != null) {
+                enterWildernessButton.interact("Enter Wilderness");
+            }if (!wilderness.contains(myPlayer().getPosition())){
+                status = ("Walking to the wilderness...");
+                WebWalkEvent webWalkEvent = new WebWalkEvent(wilderness);
+                webWalkEvent.setBreakCondition(new Condition() {
+                    @Override
+                    public boolean evaluate() {
+                        return getWidgets().getWidgetContainingText("Enter Wilderness") != null;
+                    }
+                });
+                getWalking().webWalk(wilderness);
+            }
+        }
